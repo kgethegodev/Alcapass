8ffee6cbd51695f05de66d410b6f6e9f65b1ecdb
Kgethi <97187036+kgethegodev@users.noreply.github.com>
2023-04-13 19:43:57 +0200

    [feat] building basic structure

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { RootStack } from "./navigators/RootStack";
+
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+
+import useFonts from "./hooks/useFonts";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [IsReady, SetIsReady] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const LoadFonts = async () => {
+    await useFonts();
+  };
+
+  if (!IsReady) {
+    return (
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => SetIsReady(true)}
+        onError={() => {}}
+      />
+    );
+  }
+  return <RootStack />;
+}
